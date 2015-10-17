e6b1fedd4524e19e673adca4702c0427f6dbf40c
Andy Priebe <priebea@comcast.net>
2015-10-16 23:17:08 -0700

    Corrected link to GH pages in README

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-$(function() {
+$(document).ready(function() {
 
   var number = parseInt(prompt("Enter a number"));
 
+
   if (number % 5 && number % 3) {
-  $('#pingpong').show("pingpong");
+  $('#pingpong').show();
 } else if (number % 5) {
   $('#pong').show("pong");
 }  else if (number % 3) {
   $('#ping').show("ping");
 }
-
-
-// } else {
-//   $('#integers').show();
-// }
   event.preventDefault();
 
 });
