689a527dc112ef1dc9323e22a2ac2a714582fe6a
Andy Priebe <priebea@comcast.net>
2015-10-16 18:39:45 -0700

    adds (apparently) working algorithm for sorting divisble inputs but html is not displaying them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-  var number = parseInt(prompt(Enter a number));
-  if (number % 5) && (number % 3) {
+  var number = parseInt(prompt("Enter a number"));
+
+  if (number % 5 && number % 3) {
   $('#pingpong').show();
-  else if (number % 5) {
+} else if (number % 5) {
   $('#pong').show();
-  else if (number % 3) {
+}  else if (number % 3) {
   $('#ping').show();
 } else {
   $('#integers').show();
-  });
-  });
+}
+  event.preventDefault();
+
+});
